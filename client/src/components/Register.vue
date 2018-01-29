2d7fd04e6c6f05519d4d127d70fdb963b0bcb47a
tahaa Bahrami nejad <t_bahraminejad@rasana.ir>
2018-01-29 17:28:33 +0330

    first commit

---
+++ client/src/components/Register.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input
+    type="email"
+    name="email"
+    v-model="email"
+    placeholder="email"
+    class="input"/>
+    <input
+    type="password"
+    name="password"
+    v-model="password"
+    placeholder="password"/>
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .input{
+    display: inline-flex;
+    margin: 15px
+  }
+</style>
