ad0aabe4f0dc1033d2364bcb6c492eb229793e81
tahabahrami <tahabahraminezhad@gmail.com>
2018-01-29 13:27:54 -0800

    create function undifined

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -13,10 +13,12 @@ fs.readdirSync(__dirname).filter(
   (file) =>
   file !== 'index.js'
 )
-.forEach(file => {
+.forEach((file) => {
   const model = sequelize.import(path.join(__dirname, file))
   db[model.name] = model
-  });
+  })
+
 db.sequelize = sequelize
 db.Sequelize = Sequelize
+
 module.exports = db
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,12 +1,4 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
 module.exports = (app) => {
-  app.post('/register',(req,res) => {
-    res.send({
-        message : `hello ${req.body.password}! your user was registered!`
-    })
-  })
-  app.get('/status' , (req , res) => {
-    res.send({
-        message : 'hello world'
-    })
-  })
+  app.post('/register',AuthenticationController.register)
 }
