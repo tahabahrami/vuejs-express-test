fbd8f4f6be00ebdfc6f4fe3e574ca6a0d1012d3e
tahaa Bahrami nejad <t_bahraminejad@rasana.ir>
2018-01-31 20:18:31 +0330

    vuetify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,54 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      right
+      app
+    >
+      <v-list dense>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="cyan" dark fixed app>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+       <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer color="cyan" app>
+      <v-spacer></v-spacer>
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: "App"
+  data: () => ({
+    drawer: false
+  }),
+  props: {
+    source: String
+  }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,22 +1,58 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input
-    type="email"
-    name="email"
-    v-model="email"
-    placeholder="email"
-    class="input"/>
-    <input
-    type="password"
-    name="password"
-    v-model="password"
-    placeholder="password"/>
-    <br>
-    <button @click="register">Register</button>
-    <br>
-  <div class="error" v-html="error"></div>
-  </div>
+<v-layout justify-center row>
+<v-flex xs3 fluid mt-5>
+  <v-toolbar class="cyan" dark>
+    <v-toolbar-title>Register</v-toolbar-title>
+    </v-toolbar>
+    <v-flex class="elevation-10" pa-5>
+      <v-alert
+      class="elevation-3 mb-3"
+      color="error"
+      icon="warning"
+      dismissible
+      v-model="isError"
+      transition="scale-transition"
+    >{{error}}</v-alert>
+      <v-text-field
+      prepend-icon="email"
+      color="teal"
+      type="email"
+      label="email"
+      :rules="[rules.required,rules.ali]"
+      v-model="email"
+      ma-3
+      ></v-text-field>
+      <v-text-field
+      prepend-icon="vpn_key"
+      hint="At least 8 characters"
+      :append-icon="e4 ? 'visibility' : 'visibility_off'"
+      :append-icon-cb="() => (e4 = !e4)"
+      :type="e4 ? 'password' : 'text'"
+      min="8"
+      max="32"
+      color="teal"
+      label="Enter your password"
+      :rules="[rules.required]"
+      v-model="password"
+      >
+      </v-text-field>
+    <v-flex xs4 offset-xs4>
+    <v-btn class="cyan mt-4 text-xs-center" dark @click="register">Register</v-btn>
+    </v-flex>
+    <v-flex>
+    <v-alert
+      color="success"
+      icon="check_circle"
+      dismissible
+      v-html="error"
+      v-show="isLogin"
+      class="elevation-10 mt-3"
+      transition="scale-transition">
+    </v-alert>
+    </v-flex>
+</v-flex>
+</v-flex>
+</v-layout>
 </template>
 
 <script>
@@ -26,19 +62,41 @@ export default {
     return {
       email: "",
       password: "",
-      error: null
+      custom: true,
+      error: null,
+      isError: false,
+      isLogin: false,
+      e4: true,
+      rules: {
+        required: value => !!value || "Required.",
+        ali: value => value.length < 10 || "kame"
+      }
     };
   },
+  computed: {
+    progress() {
+      return Math.min(100, this.password.length * 10);
+    },
+    color() {
+      return ["error", "warning", "success"][Math.floor(this.progress / 40)];
+    }
+  },
   methods: {
+    loginAlert() {
+      return this.isLogin;
+    },
     async register() {
       try {
         const response = await AuthenticationService.register({
           email: this.email,
           password: this.password
         });
+        this.isError = false;
+        this.isLogin = true;
+        this.error = "you loged in successfully";
         console.log(response.data);
-        this.error = "";
       } catch (error) {
+        this.isError = true;
         this.error = error.response.data.error;
       }
     }
@@ -48,13 +106,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.input {
-  display: inline-flex;
-  margin: 15px;
-}
-.error {
-  color: tomato;
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-  font-size: 20px;
-}
+
 </style>
